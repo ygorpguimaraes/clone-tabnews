5ce294f801980a00e81d58f3c1b615ce3830e99f
ygorpguimaraes <83374120+ygorpguimaraes@users.noreply.github.com>
2023-06-26 01:16:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do TabNews sendo reconstruído para aprendizagem no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Mudei aqui para o texto certo</h1>
+function Home() {
+  return <h1>Mudei aqui para o texto certo</h1>;
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
